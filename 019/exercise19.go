37e05a5fd6d55c04697276d4336e60cdeff01852
Shourai <10200748+Shourai@users.noreply.github.com>
2022-02-07 09:56:30 +0100

    Add exercise 19

---
+++ 019/exercise19.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"100-golang-exercises/utils"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type person struct {
+	name  string
+	age   int
+	score int
+}
+
+func main() {
+	input := utils.GetUserInput()
+	slice := strings.Split(input, " ")
+
+	var people []person
+
+	for _, p := range slice {
+		var person person
+		info := strings.Split(p, ",")
+		person.name = info[0]
+		person.age, _ = strconv.Atoi(info[1])
+		person.score, _ = strconv.Atoi(info[2])
+		people = append(people, person)
+	}
+
+	sort.Slice(people, func(i, j int) bool {
+		if people[i].name < people[j].name {
+			return true
+		} else if people[i].name > people[j].name {
+			return false
+		} else if people[i].age > people[j].age {
+			return true
+		}
+		return people[i].score > people[j].score
+	})
+
+	fmt.Println(people)
+
+}
